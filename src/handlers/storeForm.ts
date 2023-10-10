cb825f3a714bf79768df2d05fd15948ddf14adf5
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-09 20:19:25 -0400

    call cosmosClient and put sample data to the DB

--- src/handlers/storeForm.ts
+++ src/handlers/storeForm.ts
@@ -1,8 +1,26 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-
+import container from './cosmosClient';
 const storeForm: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log('Typescript HTTP trigger function processed a request.');
 
+  const item = {
+    id: "unique-item-id",
+    name: "John Doe",
+    age: 30,
+  };
+
+  try {
+    const { resource: createdItem } = await container.items.create(item);
+    context.res = {
+      status: 200,
+      body: createdItem,
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message,
+    };
+  }
   if (req.query.name || (req.body?.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
