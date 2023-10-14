0830c3137ac6605803cccf6fe805834c4d710d8d
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-14 15:12:33 -0400

    add basic cosmosdb config

--- src/setForm.ts
+++ src/setForm.ts
@@ -1,11 +1,36 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { CosmosClient } from "@azure/cosmos";
+
+const cosmosDBConnectionString = process.env.CosmosDBConnectionString;
+const databaseId = "YourDatabaseId";
+const containerId = "YourContainerId";
 
 export async function setForm(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
+    context.log(process.env)
+    
+    const cosmosClient = new CosmosClient(cosmosDBConnectionString);
+    const container = cosmosClient.database(databaseId).container(containerId);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+    // Define the item you want to insert into Cosmos DB
+    const newItem = {
+        id: Date.now().toString(), // You should use a unique ID for your items
+        name: name,
+        // Add other properties as needed
+    };
 
-    return { body: `Hello, ${name}!` };
+    try {
+        const { resource } = await container.items.create(newItem);
+        return {
+            status: 200,
+            body: `Item inserted with ID: ${resource.id}`,
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            body: `Error inserting item into Cosmos DB: ${error.message}`,
+        };
+    }
 };
 
 app.http('form', {
