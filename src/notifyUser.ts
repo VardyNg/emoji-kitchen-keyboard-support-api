fa58ef515e4b90e77ed7135619619611e8198868
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-15 17:13:09 -0400

    add notifyUser function and cosmosdb triggeR

---
+++ src/notifyUser.ts
@@ -0,0 +1,20 @@
+import {
+  app,
+  InvocationContext,
+} from "@azure/functions";
+
+export async function notfiyUser(documents: unknown[], context: InvocationContext): Promise<void> {
+  console.log('notifyUser called');
+  console.log(documents);
+  console.log(context);  
+
+};
+
+app.cosmosDB('cosmosDBTrigger', {
+  connection: 'e2eTest_cosmosDB',
+  databaseName: process.env.FORM_DATABASE_ID,
+  containerName: process.env.FORM_CONTAINER_ID,
+  createLeaseContainerIfNotExists: true,
+  leaseContainerPrefix: '1',
+  handler: notfiyUser,
+});
